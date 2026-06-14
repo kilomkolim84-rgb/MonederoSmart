ae12209cb51e35fd87d31c972ab2f8dc58dba3b0
kilomkolim84-rgb <kilomkolim84@gmail.com>
2026-06-13 22:34:40 -0500

    Update Moneda.kt

--- app/src/main/java/com/kilomkolim84rgb/monedero/Moneda.kt
+++ app/src/main/java/com/kilomkolim84rgb/monedero/Moneda.kt
@@ -1,1 +1,12 @@
 
+package com.kilomkolim84rgb.monedero
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Moneda(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val valor: Double,
+    val timestamp: Long = System.currentTimeMillis()
+)
