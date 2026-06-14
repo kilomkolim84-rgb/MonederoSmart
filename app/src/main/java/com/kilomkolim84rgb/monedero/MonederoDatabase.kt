fd772a6ea56c8d9871dd3fc635a3b28ae8cbccdd
kilomkolim84-rgb <kilomkolim84@gmail.com>
2026-06-14 16:10:27 -0500

    Delete app/src/main/java/com/kilomkolim84rgb/monedero/MonederoDatabase.kt

--- app/src/main/java/com/kilomkolim84rgb/monedero/MonederoDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.kilomkolim84rgb.monedero
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Transaccion::class], version = 1, exportSchema = false)
-abstract class MonederoDatabase : RoomDatabase() {
-    abstract fun transaccionDao(): TransaccionDao
-}
