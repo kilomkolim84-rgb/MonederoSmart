6df6167d4de7854b063ab91b04c3e63ad54ddd1b
kilomkolim84-rgb <kilomkolim84@gmail.com>
2026-06-13 22:36:50 -0500

    Update MonedaDao.kt

--- app/src/main/java/com/kilomkolim84rgb/monedero/MonedaDao.kt
+++ app/src/main/java/com/kilomkolim84rgb/monedero/MonedaDao.kt
@@ -1,1 +1,19 @@
 
+package com.kilomkolim84rgb.monedero
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MonedaDao {
+    @Insert
+    suspend fun insertar(moneda: Moneda)
+
+    @Query("SELECT * FROM Moneda ORDER BY timestamp DESC")
+    fun obtenerTodas(): Flow<List<Moneda>>
+
+    @Query("SELECT SUM(valor) FROM Moneda")
+    fun obtenerTotal(): Flow<Double?>
+}
